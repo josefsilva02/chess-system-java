fe4dcf32d9c56ea4e9bec7dfdd0ed3e9b46c0ec1
josef santos <josefsilva02@outlook.com>
2023-06-15 16:06:31 -0300

    Criado board exception e feito progamacao defensiva

--- src/jogoTabuleiro/Tabuleiro.java
+++ src/jogoTabuleiro/Tabuleiro.java
@@ -8,6 +8,10 @@ public class Tabuleiro {
 	
 	
 	public Tabuleiro(int linhas, int colunas) {
+		
+		if (linhas < 1 || colunas < 1) {
+			throw new TabuleiroExc("Erro criando tabuleiro , neccessario que tenha 1 linha e 1 coluna!");
+		}
 		this.linhas = linhas;
 		this.colunas = colunas;
 		pecas = new Peca[linhas][colunas];
@@ -19,22 +23,15 @@ public class Tabuleiro {
 	}
 
 
-	public void setLinhas(int linhas) {
-		this.linhas = linhas;
-	}
-
-
 	public int getColunas() {
 		return colunas;
 	}
 
-
-	public void setColunas(int colunas) {
-		this.colunas = colunas;
-	}
-	
 	
 	public Peca peca(int linha , int coluna) {
+		if(!positionExist(linha, coluna)) {
+			throw new TabuleiroExc("Posicao nao existe");
+		}
 		return this.pecas[linha][coluna];
 		
 		
@@ -42,14 +39,39 @@ public class Tabuleiro {
 	
 	
 	public Peca peca(Posicao posicao) {
+		if(!posicaoExiste(posicao)) {
+			throw new TabuleiroExc("Posicao nao existe");
+		}
 		
 		return pecas[posicao.getLinha()][posicao.getColuna()];
 		
 	}
 	
 	public void colocarPeca(Peca peca , Posicao posicao) {
+		
+		
+		if(haUmaPeca(posicao)) {
+			throw new TabuleiroExc("Existe uma peca nessa posicao" + posicao);
+		}
 		pecas[posicao.getLinha()][posicao.getColuna()] = peca;
 		peca.posicao = posicao;
 	}
-
+	
+	private boolean positionExist(int linha , int coluna ) {
+		return linha >= 0 && linha < linhas && coluna >= 0 && coluna < colunas;
+	}
+	
+	public boolean posicaoExiste(Posicao posicao) {
+		return positionExist(posicao.getLinha() , posicao.getColuna());
+	}
+	
+	
+	public boolean  haUmaPeca(Posicao posicao) {
+		if(!posicaoExiste(posicao)) {
+			throw new TabuleiroExc("Posicao nao existe");
+		}
+		return peca(posicao) != null;
+		
+		
+	}
 }
---
+++ src/jogoTabuleiro/TabuleiroExc.java
@@ -0,0 +1,11 @@
+package jogoTabuleiro;
+
+public class TabuleiroExc extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public TabuleiroExc(String msg) {
+		super(msg);
+	}
+
+}
